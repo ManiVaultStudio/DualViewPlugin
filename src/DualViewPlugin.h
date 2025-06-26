22f64d0a2b36ce2227c50cab1e830730b8f8851a
Chang <c.li1@lumc.nl>
2025-06-26 13:58:05 +0200

    Update plugin config file

--- src/DualViewPlugin.h
+++ src/DualViewPlugin.h
@@ -261,7 +261,7 @@ class DualViewPluginFactory : public ViewPluginFactory
     Q_INTERFACES(mv::plugin::ViewPluginFactory mv::plugin::PluginFactory)
     Q_OBJECT
     Q_PLUGIN_METADATA(IID   "studio.manivault.DualViewPlugin"
-                      FILE  "DualViewPlugin.json")
+                      FILE  "PluginInfo.json")
 
 public:
 
